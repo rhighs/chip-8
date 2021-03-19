006d0b62dbb01d666af502ca1401cf9e2eb55938
Roberto Montalti <roberto.montalti3@studio.unibo.it>
2021-03-19 05:51:56 +0100

    added: load_rom method

--- chip8.hpp
+++ chip8.hpp
@@ -1,17 +1,18 @@
 #include <cstdint>
 
-class Chip8
-{
-public:
-	uint8_t registers[16]{};
-	uint8_t memory[4096]{};
-	uint16_t index{};
-	uint16_t pc{};
-	uint16_t stack[16]{};
-	uint8_t sp{};
-	uint8_t delayTimer{};
-	uint8_t soundTimer{};
-	uint8_t keypad[16]{};
-	uint32_t video[64 * 32]{};
-	uint16_t opcode;
+class Chip8{
+    public:
+        uint8_t registers[16]{};
+        uint8_t memory[4096]{};
+        uint16_t index{};
+        uint16_t pc{};
+        uint16_t stack[16]{};
+        uint8_t sp{};
+        uint8_t delay_timer{};
+        uint8_t sound_timer{};
+        uint8_t keypad[16]{};
+
+        /*each value is either set to 1 or 0*/
+        uint32_t video[64 * 32]{};
+        uint16_t opcode;
 };
