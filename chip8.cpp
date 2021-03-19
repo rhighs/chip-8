be08040dcb9cedce4cf3a808fdcfea22dae46e41
robertomontalti <rob@LAPTOP-BJ0L34C1.localdomain>
2021-03-19 14:50:13 +0100

    edit: if statement in rom loader

--- chip8.cpp
+++ chip8.cpp
@@ -4,25 +4,21 @@
 const unsigned int START_ADDR = 0x200;
 
 void Chip8::load_rom(const char* filename){
-
-
     std::ifstream file(filename, std::ios::binary, std::ios::ate);
 
-    if(file.is_open()){
+    if(!file.is_open()) return;
 
-        //allocates a buffer to held the rom
-        std::streampos size = file.tellg();
-        char* buffer = new char[size];
+    //allocates a buffer to held the rom
+    std::streampos size = file.tellg();
+    char* buffer = new char[size];
 
-        //reads and saves to buffer
-        file.seekg(0, ios::beg);
-        file.read(buffer, size);
+    //reads and saves to buffer
+    file.seekg(0, ios::beg);
+    file.read(buffer, size);
 
-        for(long i = 0; i < size; ++i)
-            memory[START_ADDR + i] = buffer[i];
+    for(long i = 0; i < size; ++i)
+        memory[START_ADDR + i] = buffer[i];
 
-        //unalloc buffer
-        delete[] buffer;
-    }
-
+    //unalloc buffer
+    delete[] buffer;
 }
