460f3d6563c490a0fe6efaf993109313a3c55712
roberto montalti <roberto.montalti00@gmail.com>
2021-08-17 17:48:36 +0200

    fixed tabs

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,16 @@
 
 using namespace sku;
 
-int main(int argc, char** argv)
-{
-	if (argc != 4)
-	{
-		std::cerr << "Usage: " << argv[0] << " <Scale> <Delay> <ROM>\n";
-		return EXIT_FAILURE;
-	}
+int main(int argc, char** argv) {
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <Scale> <Delay> <ROM>\n";
+        return EXIT_FAILURE;
+    }
+    int videoScale = std::stoi(argv[1]);
+    int cycleDelay = std::stoi(argv[2]);
+    char const* romFilename = argv[3];
 
-	int videoScale = std::stoi(argv[1]);
-	int cycleDelay = std::stoi(argv[2]);
-	char const* romFilename = argv[3];
-
-	Chip8 chip8;
+    Chip8 chip8;
     Video platform(
             "CHIP-8 Emulator",
             chip8.VIDEO_WIDTH * videoScale,
@@ -25,26 +22,23 @@ int main(int argc, char** argv)
             chip8.VIDEO_WIDTH,
             chip8.VIDEO_HEIGHT
             );
-	chip8.load_rom(romFilename);
+    chip8.load_rom(romFilename);
 
-	int videoPitch = sizeof(chip8.video[0]) * chip8.VIDEO_WIDTH;
-	auto lastCycleTime = std::chrono::high_resolution_clock::now();
-	bool quit = false;
+    int videoPitch = sizeof(chip8.video[0]) * chip8.VIDEO_WIDTH;
+    auto lastCycleTime = std::chrono::high_resolution_clock::now();
+    bool quit = false;
 
-	while (!quit)
-	{
-		quit = platform.process_input(chip8.keypad);
+    while (!quit) {
+        quit = platform.process_input(chip8.keypad);
 
-		auto currentTime = std::chrono::high_resolution_clock::now();
-		float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastCycleTime).count();
+        auto currentTime = std::chrono::high_resolution_clock::now();
+        float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastCycleTime).count();
 
-		if (dt > cycleDelay)
-		{
-			lastCycleTime = currentTime;
-			chip8.cycle();
-			platform.update(chip8.video, videoPitch);
-		}
-	}
-
-	return 0;
+        if (dt > cycleDelay) {
+            lastCycleTime = currentTime;
+            chip8.cycle();
+            platform.update(chip8.video, videoPitch);
+        }
+    }
+    return 0;
 }
